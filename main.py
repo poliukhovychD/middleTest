2961420fdba07b005f5b4bce8c63c3ec658e444d
poliukhovychD <f0rgetz@knu.ua>
2023-03-28 14:28:39 +0300

    filter_file_by_keywords_data implemented

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+def filter_file_by_keywords_data():
+    input_filename = "exam.txt"
+    keywords_str = "lol, dis"
+    keywords = keywords_str.split(",")
+
 def main():
+    filter_file_by_keywords_data()
 
 
 if __name__ == "__main__":
