7a6974f5e5373a76c602c3d96baecbde766c8b99
poliukhovychD <f0rgetz@knu.ua>
2023-03-28 14:39:31 +0300

    writing to file method implemented

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def filter_file_by_keywords(input_filename, keywords):
     filtered_lines = [line for line in lines if any(keyword in line for keyword in keywords)]
 
     filtered_string = ''.join(filtered_lines)
+    write_to_file(filtered_string)
 
     print(f"Filtered lines containing any of the keywords: {keywords}")
     for line in filtered_lines:
@@ -12,11 +13,15 @@ def filter_file_by_keywords(input_filename, keywords):
 
     print(f"\nTotal lines containing any of the keywords: {len(filtered_lines)}")
 
+def write_to_file(string):
+    with open('filtered.txt', 'w') as output_file:
+        output_file.write(string)
+
 def filter_file_by_keywords_data():
-    input_filename = "exam.txt"
-    keywords_str = "lol, dis"
+    input_filename = "data.txt"
+    keywords_str = "lol, display"
     keywords = keywords_str.split(",")
-    filter_file_by_keywords("exam.txt", keywords)
+    filter_file_by_keywords(input_filename, keywords)
 
 def main():
     filter_file_by_keywords_data()
