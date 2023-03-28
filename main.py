1ec95bd9601a2affd43e3a86a3695d872fdabb1a
poliukhovychD <f0rgetz@knu.ua>
2023-03-28 14:35:13 +0300

    filter file method implemented

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+def filter_file_by_keywords(input_filename, keywords):
+    with open(input_filename, 'r') as input_file:
+        lines = input_file.readlines()
+
+    filtered_lines = [line for line in lines if any(keyword in line for keyword in keywords)]
+
+    filtered_string = ''.join(filtered_lines)
+
+    print(f"Filtered lines containing any of the keywords: {keywords}")
+    for line in filtered_lines:
+        print(line.strip())
+
+    print(f"\nTotal lines containing any of the keywords: {len(filtered_lines)}")
+
 def filter_file_by_keywords_data():
     input_filename = "exam.txt"
     keywords_str = "lol, dis"
     keywords = keywords_str.split(",")
+    filter_file_by_keywords("exam.txt", keywords)
 
 def main():
     filter_file_by_keywords_data()
